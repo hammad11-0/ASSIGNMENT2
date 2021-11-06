6a011ca77a75d745a6d5820124cfb4d0d377fc4e
hammad11-0 <bitf18a504@pucit.edu.pk>
2021-11-06 02:43:58 -0700

    editTextnumber

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText editTextNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        editTextNumber =findViewById(R.id.editTextNumber);
+
     }
+
+
+
+
+
+
+
 }
